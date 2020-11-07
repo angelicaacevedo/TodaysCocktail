fb5e1abd0f54619b963b44dbac62987b1de8d1da
Angélica Acevedo <aceroacevedoangelica@gmail.com>
2020-11-06 21:08:15 -0300

    Adição do Retrofit para conetar com API dos cocktails

--- app/src/main/java/com/example/todayscocktail/network/Cocktail.kt
+++ app/src/main/java/com/example/todayscocktail/network/Cocktail.kt
@@ -1,7 +1,12 @@
 package com.example.todayscocktail.network
 
+import com.squareup.moshi.Json
+
 class Cocktail (
+    @Json(name = "idDrink")
     val id: Int,
+    @Json(name = "idDrink")
     val name: String,
+    @Json(name = "idDrink")
     val thumbUrl: String
 )
